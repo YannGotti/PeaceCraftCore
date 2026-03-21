3a6df9465763006f6c131132c3cb4153d6299ba2
mironoouv <1111111111aleksey46@gmail.com>
2026-03-21 20:51:05 +0300

    init project

---
+++ src/client/java/ru/peacecraft/grandline/PeaceCraftClient.java
@@ -0,0 +1,10 @@
+package ru.peacecraft.grandline;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class PeaceCraftClient implements ClientModInitializer {
+	@Override
+	public void onInitializeClient() {
+		// This entrypoint is suitable for setting up client-specific logic, such as rendering.
+	}
+}
